524a964d7417b2e8581668f76a790310d864b39a
diego1soler <dsoler1998@gmail.com>
2016-07-24 12:51:29 -0600

    Domingo

--- src/I_Calculadora.java
+++ src/I_Calculadora.java
@@ -1,5 +1,9 @@
 
-public class I_Calculadora {
+public interface I_Calculadora {
 
+	
+	public int Calcular(String vector);
+	public String toString();
+	public String LeerArchivo (String direccion);
 }
- //Holaj
+ 
