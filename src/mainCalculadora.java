dbe959ada24389fd358c415f0bc2f0313bcac67d
diego1soler <dsoler1998@gmail.com>
2016-07-24 22:42:30 -0600

    Main

--- src/mainCalculadora.java
+++ src/mainCalculadora.java
@@ -3,9 +3,10 @@ public class mainCalculadora {
 
 	public static void main(String[] args) {
 
-		System.out.println("Bienvenido invocador a la calculadora POST-FIX.\n Usted habrá");
+		System.out.println("Bienvenido invocador a la calculadora POST-FIX.\nUsted habrá llenado el respectivo archivo con las operaciones.");
 		Calculadora Calculadora = new Calculadora();
-		System.out.println("La calculadora ha hecho magia. El resultado es: "+Calculadora.Calcular(Calculadora.LeerArchivo("datos.txt")));
+		String cadena = Calculadora.LeerArchivo("datos.txt");
+		System.out.println("La calculadora ha hecho magia. El resultado es: "+ Calculadora.Calcular(cadena));
 		
 		
 	}
